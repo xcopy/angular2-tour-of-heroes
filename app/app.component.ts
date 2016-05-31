7d4a51b69f95ab783c48d00fe97f084b3d282458
Kairat Jenishev <kairat.jenishev@gmail.com>
2016-05-31 13:47:16 +0600

    Tour of Heroes: Part 3

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail.component';
+
 
 export class Hero {
     id: number;
@@ -17,14 +19,7 @@ export class Hero {
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
-        <div *ngIf="selectedHero">
-            <h2>{{selectedHero.name}} details!</h2>
-            <div><label>id: </label>{{selectedHero.id}}</div>
-            <div>
-                <label>name: </label>
-                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-            </div>
-        </div>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `,
     styles: [`
       .selected {
@@ -74,7 +69,8 @@ export class Hero {
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-    `]
+    `],
+    directives: [HeroDetailComponent]
 })
 
 export class AppComponent {
